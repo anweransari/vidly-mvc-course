1e94d39d2bf17be38743155877680410fb7295fb
anwer ansari <anweransari99@gmail.com>
2020-04-28 16:52:09 -0400

    added comments

--- Vidly/ViewModels/RandomMovieViewModel.cs
+++ Vidly/ViewModels/RandomMovieViewModel.cs
@@ -8,6 +8,7 @@ namespace Vidly.ViewModels
 {
     public class RandomMovieViewModel
     {
+        //no changes made to this model
         public Movie Movie { get; set; }
         public List<Customer> Customers { get; set; }
     }
